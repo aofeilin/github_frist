df1c17bcf9c55b7bcff82c506d8f37da8510b475
aofeilin <aofeilin@163.com>
2017-01-20 16:52:10 +0800

    简介 串串
    描述 串串

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github_frist
+//
+//  Created by ule_zhangfanglin on 2017/1/20.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github_frist
+//
+//  Created by ule_zhangfanglin on 2017/1/20.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
